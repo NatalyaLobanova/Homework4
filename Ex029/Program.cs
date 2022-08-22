136cd88670ae09a9eb160335dfc449a73222ecf7
Natalya Lobanova <knopka40a@mail.ru>
2022-08-22 19:51:59 +0500

    Add Ex029

---
+++ Ex029/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из N элементов 
+// и выводит их на экран.
+// 5 -> [1, 2, 5, 7, 19]
+// 3 -> [6, 1, 33]
+
+int[] numbers = new int[5];
+FillArray(numbers);
+PrintArray(numbers);
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    { 
+        array[i] = new Random().Next(1, 100);
+    }
+}
+void PrintArray(int[] array)
+{
+     for(int i = 0; i < array.Length; i++)
+     {
+        Console.Write(array[i] + " ");
+     }
+}
+
